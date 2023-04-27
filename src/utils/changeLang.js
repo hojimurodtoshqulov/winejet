47dc7ac0161b01b674a6b9635445182923080cee
diyorbek-yusupov <dyuzb2004@gmail.com>
2023-04-27 16:51:26 +0500

    nothing changed

--- src/utils/changeLang.js
+++ src/utils/changeLang.js
@@ -2,5 +2,5 @@ import i18next from "i18next";
 
 export function getContent(content_ru, content_uz) {
   const currentLanguage = i18next.language;
-  return currentLanguage === "ru" ? content_ru : content_uz;
+  return currentLanguage === "uz" ? content_uz : content_ru;
 }
