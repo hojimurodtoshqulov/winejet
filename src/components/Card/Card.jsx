e4434783b4f9d7b5391157344d6149029a0b1cbc
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-04-29 00:33:03 +0500

    uodatedZ

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -24,7 +24,7 @@ const Card = ({
 
     return shortenedString;
   }
-  // console.log(`/courses/${link}`);
+  console.log(`/courses/${link}`);
   return (
     <Link to={`/courses/${link}`} className="card-component">
       <div>
