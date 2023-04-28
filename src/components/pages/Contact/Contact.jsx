11568ecb902337311a0c55c37859911534defb92
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-04-28 11:57:15 +0500

    updated

--- src/components/pages/Contact/Contact.jsx
+++ src/components/pages/Contact/Contact.jsx
@@ -12,83 +12,97 @@ import CustomMap from "../../CustomMap/CustomMap";
 import Card from "../../Card/Card";
 
 let DefaultIcon = L.icon({
-  iconUrl: icon,
-  shadowUrl: iconShadow,
+	iconUrl: icon,
+	shadowUrl: iconShadow,
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
 const MyMap = () => {
-  const position = [41.299496, 69.240074];
+	const position = [41.299496, 69.240074];
 
-  return (
-    <MapContainer
-      center={position}
-      style={{
-        width: "100%",
-        height: "100%",
-        zIndex: "40",
-      }}
-      zoom={13}
-      scrollWheelZoom={true}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}></Marker>
-    </MapContainer>
-  );
+	return (
+		<MapContainer
+			center={position}
+			style={{
+				width: "100%",
+				height: "100%",
+				zIndex: "40",
+			}}
+			zoom={13}
+			scrollWheelZoom={true}
+		>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+			<Marker position={position}></Marker>
+		</MapContainer>
+	);
 };
 
 const Contact = () => {
-  return (
-    <section className="contact-page">
-      <div className="contact-page__container">
-        <h1 data-aos="fade-down" className="contact-page__title">
-          КОНТАКТЫ
-        </h1>
-        <div className="contact-page__wrapper">
-          <div className="contact-page__info-wrapper">
-            <div data-aos="fade-left">
-              <p>
-                <BsTelephone />
-              </p>
-              <p>+998 95 052 46 26</p>
-            </div>
-            <div data-aos="fade-right">
-              <p>
-                <MdOutlineEmail />
-              </p>
-              <p>nurmuhammad2003o@gmail.com</p>
-            </div>
-            <div data-aos="fade-left">
-              <p>
-                <IoLocationOutline />
-              </p>
-              <p>Tashkent, Sergeli</p>
-            </div>
-          </div>
-          <div className="contact-page__inputs">
-            <h1 data-aos="fade-up">Fill in form</h1>
-            <div>
-              <input data-aos="fade-right" type="text" placeholder="Name" />
-              <input data-aos="fade-left" type="text" placeholder="Email" />
-              <textarea
-                data-aos="fade-right"
-                rows="3"
-                placeholder="Message"
-              ></textarea>
-              <button data-aos="fade-down">send</button>
-            </div>
-          </div>
-        </div>
-        <div data-aos="zoom-in" className="contact-page__map-wrapper">
-          <CustomMap />
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className="contact-page">
+			<div className="contact-page__container">
+				<h1 data-aos="fade-down" className="contact-page__title">
+					КОНТАКТЫ
+				</h1>
+				<div className="contact-page__wrapper">
+					<div className="contact-page__info-wrapper">
+						<div data-aos="fade-left">
+							<p>
+								<BsTelephone />
+							</p>
+							<p>
+								<a href="tel:+998999999999" target="_blanck">
+									+998 99 999 99 99
+								</a>
+							</p>
+						</div>
+						<div data-aos="fade-right">
+							<p>
+								<MdOutlineEmail />
+							</p>
+							<p>
+								<a href="mail:winejet-uz@gmail.com" target="_blanck">
+									winejet-uz@gmail.com
+								</a>
+							</p>
+						</div>
+						<div data-aos="fade-left">
+							<p>
+								<IoLocationOutline />
+							</p>
+							<p>
+								<a href="">Tashkent, Sergeli</a>
+							</p>
+						</div>
+					</div>
+					<div className="contact-page__inputs">
+						<h1 data-aos="fade-up">Заполните форму</h1>
+						<div>
+							<input data-aos="fade-right" type="text" placeholder="Имя" />
+							<input
+								data-aos="fade-left"
+								type="text"
+								placeholder="Электронная почта"
+							/>
+							<textarea
+								data-aos="fade-right"
+								rows="3"
+								placeholder="Сообщение"
+							></textarea>
+							<button data-aos="fade-down">Oтправить</button>
+						</div>
+					</div>
+				</div>
+				<div data-aos="zoom-in" className="contact-page__map-wrapper">
+					<CustomMap />
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Contact;
